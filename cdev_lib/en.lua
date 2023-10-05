43f866f3b12955e9b5238ab9799ea0027757aea3
Newtonzz <53558337+Newtonzz@users.noreply.github.com>
2023-10-05 10:07:10 +0200

    Update en.lua

--- cdev_lib/en.lua
+++ cdev_lib/en.lua
@@ -2,4 +2,9 @@ clib_Localizer.append("en", {
     go_back = "Go back",
     info = "Info",
     warning = "Warning",
+    browse_categories = "Browse Categories",
+    okay_buttom = "Okay",
+    cancel_buttom = "Cancel",
+    sign_currency = "$",
+    input_value = "Input Value Here",
 })
